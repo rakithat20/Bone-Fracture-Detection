9174830ff1857fbc70343cc589eaf77aa2c80aaf
Rakitha <tharupathir@gmail.com>
2024-10-05 16:23:09 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+from img import Img
+
+image = Img(r"C:\Users\ASUS\Pictures\broken-leg-xray.jpg",'gray')
+img = image.getImg()
+
+cv2.imshow("Display window",img)
+alpha = -1.6
+beta = 0
+
+adjusted = cv2.convertScaleAbs(img,alpha=alpha,beta=beta)
+cv2.imshow("Display window",adjusted)
+k = cv2.waitKey(0)
